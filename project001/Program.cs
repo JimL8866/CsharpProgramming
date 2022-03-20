9c330f3c776f8c0ae4c803aab9a785128a88a59e
Jim <chengxuyuan9527@gmail.com>
2022-03-20 13:51:20 +1100

    Add project files.

---
+++ project001/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace project001
+{
+    class Program
+    {
+        static void Main(String[]args)
+        {
+            //int a = 4;
+            //int b = 3;
+            //a = a * a;
+            //a = a * b;
+            //Console.WriteLine($"{a}and{b}");
+            //Console.WriteLine(@"c:\\a\\b""\\c");
+            //string str=Console.ReadLine();
+            //Console.WriteLine(str+"-");
+
+        }
+
+    }
+}
