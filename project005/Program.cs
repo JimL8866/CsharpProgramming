89e3c8905f016c9c1d0518baba14c8647c0eb06b
Jim <chengxuyuan9527@gmail.com>
2022-04-18 15:46:04 +1000

    more practice

--- project005/Program.cs
+++ project005/Program.cs
@@ -29,10 +29,51 @@ namespace project005
             //int people = Convert.ToInt32(Console.ReadLine());
             //double moneyForEach = money / people;
             //Console.WriteLine(moneyForEach);
-            int a = 33;
-            int b = 43;
-            int c = a + b;
-            Console.WriteLine("the sum of {0} + {1} is {2}", a, b, c);  //格式化字符串
+            //int a = 33;
+            //int b = 43;
+            //int c = a + b;
+            //Console.WriteLine("the sum of {0} + {1} is {2}", a, b, c);  //格式化字符串
+            //Console.WriteLine(45 / 10.0); //4.5
+            //Console.WriteLine(45 / 10); //4
+            //int a = 45 / 10;
+            //Console.WriteLine(a); //4
+            //double b = 45 / 10;
+            //Console.WriteLine(b);   //4
+            //double c = 45 / 10.0;
+            //Console.WriteLine(c);  //4.5
+            //double e = (double)45 / 10;
+            //Console.WriteLine(e);  //4.5
+
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //int tens = a / 10;
+            //int ones = a % 10;
+            //Console.WriteLine(tens);
+            //Console.WriteLine(ones);
+            //int a = 5;
+            //int b = ++a;
+            //Console.WriteLine(b); //++放在前面，先增加在赋值。不管怎样a最后都会增加
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //int thousands = a / 100;
+            //int remainder = a % 100;
+            //int tens = remainder / 10;
+            //int ones = remainder % 10;
+            //Console.WriteLine("result is {0}{1}{2}", ones, tens, thousands);
+            //int a = 10;
+            //int b = 20;
+            //int c = 30;
+            //Console.WriteLine("" + a + b + c);
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //int tens = (a / 10) % 10;
+            //int thousands = (a / 1000) % 10;
+            //int res = thousands * 10 + tens;
+            //char res1 = (char)res;
+            //Console.WriteLine(res1);
+            int a = 3;
+            int b = a++ + ++a;  //a 先赋值3，然后自增到4然后自增到5，最后加起来赋值给b
+            Console.WriteLine(a); //5
+            Console.WriteLine(b); //8
+
+
 
 
         }
