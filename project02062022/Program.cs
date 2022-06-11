586191e6846b7ce36a17f9783b8192b5701afac1
Jim <chengxuyuan9527@gmail.com>
2022-06-11 10:29:14 +1000

    more practice

---
+++ project02062022/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace project02062022
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class Program
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            #region
+            //Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            //Console.ReadKey();
+            #endregion
+            Console.WriteLine("What is your username?");
+            string permission = Console.ReadLine();
+            int level = 55;
+            int level2 = 20;
+            Console.WriteLine("What is your user level?");
+            int userLevel = Convert.ToInt32(Console.ReadLine());
+            if (permission.Contains("Admin")){
+                Console.WriteLine(userLevel > level ? "Welcome, Super Admin User" : "Welcome, Admin User");
+            }else if (permission.Contains("Manager"))
+            {
+                Console.WriteLine(userLevel >= level2 ? "Contact an Admin for access" : "You do not have sufficient privileges");
+            }else
+            {
+                Console.WriteLine("You do not have sufficient privileges");
+            }
+
+
+
+
+        }
+    }
+}
